f2cb84c343b7d8768448687376312fb6c3028166
jperaudon <j.peraudon@it-students.fr>
2021-06-16 14:59:01 +0000

    correction exo 4

--- controllers/login.php
+++ controllers/login.php
@@ -2,50 +2,33 @@
 session_start();
 
 // Exo 3 :
-$username = 'jon';
-$password = '1234';
+// $username = 'jon';
+// $password = '1234';
 
 // Exo 4 : 
-// $users = [
-//     [
-//         'name' => 'jon',
-//         'password' => '1234'
-//     ],
-//     [
-//         'name' => 'jane',
-//         'password' => '0000'
-//     ],
-//     [
-//         'name' => 'martin',
-//         'password' => 'azerty'
-//     ],
-// ]
-
-
-if ($_POST['name'] == $username && $_POST['password'] == $password) {
-    $_SESSION['pseudo'] = $_POST['name']; // Logique : l'utilisateur est connecté !
-    $_SESSION['isConnected'] = true;
-    header('Location: /');
-    exit();
-} else {
-    header('Location: /connection.php');
-    exit();
-}
-
+$users = [
+    [
+        'name' => 'jon',
+        'password' => '1234'
+    ],
+    [
+        'name' => 'jane',
+        'password' => '0000'
+    ],
+    [
+        'name' => 'martin',
+        'password' => 'azerty'
+    ],
+];
 
 foreach ($users as $user) {
-    //var_dump($_POST); die;
-    //echo $user["name_id"];
-    //echo $user["password"];
-    $_SESSION["pseudo"] = $_POST["name_id"];
-    header("Location: / ");
-    exit();
+    if ($_POST['name'] == $user['name'] && $_POST['password'] == $user['password']) {
+        $_SESSION['pseudo'] = $_POST['name'];   // Logique : nom de session de l'utilisateur
+        $_SESSION['isConnected'] = true;        // Logique : l'utilisateur est connecté !
+        header('Location: /');
+        exit();
+    }
 }
-    
-if($_POST["name_id"] != $user["name_id"] && $_POST["password"] != $user["password"]) {
-    header("Location: /connexion.php");
-    exit(); 
-} else {
-    header("Location: /connexion.php");
-    exit(); 
-} ; 
+
+header('Location: /connection.php');
+exit();
